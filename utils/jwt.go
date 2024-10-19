14f16155cef408e238ec256a57feedc03fecb461
Esa <ehsan_amd72@hotmail.com>
2024-10-19 19:37:37 +0300

    create jwt util

---
+++ utils/jwt.go
@@ -0,0 +1,24 @@
+package utils
+
+import (
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+var jwtSecret = []byte("secret_key")
+
+func GenerateToken(username string) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"username": username,
+		"exp":      time.Now().Add(time.Hour * 72).Unix(),
+	})
+
+	return token.SignedString(jwtSecret)
+}
+
+func ValidateToken(tokenString string) (*jwt.Token, error) {
+	return jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		return jwtSecret, nil
+	})
+}
